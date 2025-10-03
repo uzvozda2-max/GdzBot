c91466fb61f1942984fa55bdd59282978523413a
uzvozda2-max <uzvozda2@gmail.com>
2025-10-03 10:57:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ dp = Dispatcher(bot)
 # Завантаження / Збереження
 # ---------------------------
 def load_json(filename, default):
-    if os.path.exists(filename):
+    try:
         with open(filename, "r", encoding="utf-8") as f:
             return json.load(f)
-    return default
+    except (FileNotFoundError, json.JSONDecodeError):
+        save_json(filename, default)
+        return default
 
 def save_json(filename, data):
     with open(filename, "w", encoding="utf-8") as f:
