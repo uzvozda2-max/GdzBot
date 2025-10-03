564bc5fbb2aa6c3daff4cb0d1edd27cb33cf9dc4
uzvozda2-max <uzvozda2@gmail.com>
2025-10-03 10:39:04 +0300

    Delete loader.py

--- loader.py
+++
@@ -1,9 +0,0 @@
-import os
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # з Render у Environment Variables
-
-bot = Bot(token=BOT_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
